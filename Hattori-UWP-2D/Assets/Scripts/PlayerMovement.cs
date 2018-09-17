ee7dcb65a5171fdf643cc26797f679b2d36c7a64
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-09-17 16:54:52 +0000

    Player restricted to camera view

--- Hattori-UWP-2D/Assets/Scripts/PlayerMovement.cs
+++ Hattori-UWP-2D/Assets/Scripts/PlayerMovement.cs
@@ -5,24 +5,32 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     // Variables
-    float maxSpeed = 3.5f;
+    public float maxSpeed = 3.5f;
+    float shipBoundaryRadius = 5f;
 
-	void Start ()
+    void Start ()
     {
 		
 	}
 
-	void Update ()
+    void Update()
     {
-        // Rotation for the ship
-        
 
-        // Movement for the ship
+        // Assign players postion to a Vector3 named pos
         Vector3 pos = transform.position;
 
-        pos.y += Input.GetAxis("Vertical") * maxSpeed *Time.deltaTime;
-        pos.x += Input.GetAxis("Horizontal") * maxSpeed * Time.deltaTime;
-
+        // Movement for the ship (Vertical)
+        pos.y += Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime;
         transform.position = pos;
-	}
+
+        // Movement for the ship (Horizontal)
+        pos.x += Input.GetAxis("Horizontal") * maxSpeed * Time.deltaTime;
+        transform.position = pos;
+
+        // Restricts the player to camera view
+        pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
 }
