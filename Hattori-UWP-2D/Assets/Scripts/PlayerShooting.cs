35772eedf640c86a65cc7a3261bc93c630c0ee6a
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-09-18 12:10:29 +0000

    Added script for shooting

---
+++ Hattori-UWP-2D/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+
+    // Variables
+    public float fireDelay = 0.1f;
+    float cooldownTimer = 0;
+
+    // Update is called once per frame
+	void Update ()
+    {
+        cooldownTimer -= Time.deltaTime;
+
+        // Shooting script
+		if(Input.GetButtonDown("Fire1") && cooldownTimer <=0)
+        {
+            cooldownTimer = fireDelay;
+        }
+	}
+}
