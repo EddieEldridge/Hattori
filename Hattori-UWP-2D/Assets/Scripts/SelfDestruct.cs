89ca7941a961f011cc5104dcff7f7b3215bdc344
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-09-18 13:18:11 +0000

    Bullets now self-destruct after they exit the screen

--- Hattori-UWP-2D/Assets/Scripts/SelfDestruct.cs
+++ Hattori-UWP-2D/Assets/Scripts/SelfDestruct.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class SelfDestruct : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Variables
+    public float timer = 1f;
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer<0)
+        {
+            Destroy(gameObject);
+        }
 	}
 }
