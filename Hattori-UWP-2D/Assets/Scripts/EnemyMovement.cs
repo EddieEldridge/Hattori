6daa7d78241a389d4fbc98f928a95e8f75045fe7
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-09-21 13:29:08 +0000

    Added enemy prefab and enemies now scrolling down the screen

---
+++ Hattori-UWP-2D/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public float fallSpeed = 8.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.down * fallSpeed * Time.deltaTime, Space.World);
+
+    }
+}
